bee78592d1735d05cb1cc151118597390e118829
Ajay <106470358+PbAjay@users.noreply.github.com>
2022-11-12 08:09:21 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -47,8 +47,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Owner", "https://www.github.com/anasty17")
+    buttons.buildbutton("ɢʀᴏᴜᴘ", "https://MasterLeechGroup")
+    buttons.buildbutton("ᴏᴡɴᴇʀ", "https://t.me/Crude_X")
     reply_markup = buttons.build_menu(2)
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
